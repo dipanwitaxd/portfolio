abd6f24025412ea763ae3245ffab58c8508ea671
DipanwitaAccacia <dipanwita.mandal@accacia.ai>
2025-10-12 13:02:44 +0530

    fix: updated logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,9 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.png" sizes="any" />
+      </head>
       <body className={` antialiased`}>
         <video autoPlay muted loop className="video">
           <source src="/video.mp4" type="video/mp4" />
